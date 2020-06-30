140dddb48b1e90a1f728c22b89ec82e0157decbd
nathan-stamand <nathansaint810@gmail.com>
2020-06-30 16:04:07 -0500

    created #getSongs() to retrieve ALL songs, regardless of user who created them.

--- src/adapters/SongsAdapter.js
+++ src/adapters/SongsAdapter.js
@@ -1,5 +1,9 @@
 class SongsAdapter {
   constructor() {
-    this.baseUrl = 'http://localhost:3000/api/v1'
+    this.baseUrl = `http://localhost:3000/api/v1/songs`;
+  }
+
+  getSongs() {
+    return fetch(this.baseUrl).then(res => res.json())
   }
 }
