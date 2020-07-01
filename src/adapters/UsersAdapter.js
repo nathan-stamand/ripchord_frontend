383bab466c0c54384a27e51c049f4d7bc48c95e5
nathan-stamand <nathansaint810@gmail.com>
2020-07-01 14:37:15 -0500

    moved #attemptCreateUser to the UsersAdapter.js file

--- src/adapters/UsersAdapter.js
+++ src/adapters/UsersAdapter.js
@@ -6,4 +6,28 @@ class UserAdapter {
   getUser(id) {
     return fetch(this.baseUrl + id).then(res => res.json())
   }
+
+  static attemptCreateUser(username, password) {
+    const userData = {
+      user: {
+        username: username,
+        password: password
+    }}
+
+    const userObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData)
+    }
+
+    return fetch(this.baseUrl, userObj)
+    .then((res) => {
+      return res.json()
+    })
+    .then(json => {
+      console.log(json)
+    })
+  }
 }
--- src/components/app.js
+++ src/components/app.js
@@ -3,38 +3,13 @@ class App {
     this.songs = new Songs();
   }
 
-
-  static attemptCreateUser(username, password) {
-    const userData = {
-      user: {
-        username: username,
-        password: password
-    }}
-
-    const userObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData)
-    }
-
-    return fetch("http://localhost:3000/api/v1/users", userObj)
-    .then((res) => {
-      return res.json()
-    })
-    .then(json => {
-      console.log(json)
-    })
-  }
-  
   addCreateUserEventListener() {
     const loginSubmitBtn = document.getElementById("sign-up").querySelector("form").lastElementChild;
     loginSubmitBtn.addEventListener("click", function(e) {
       e.preventDefault();
       const username = document.getElementById("sign-up").querySelector("form").firstElementChild.value;
       const password = document.getElementById("sign-up").querySelector("form").children[3].value;
-      App.attemptCreateUser(username, password)
+      UserAdapter.attemptCreateUser(username, password)
     })
   }
 }
