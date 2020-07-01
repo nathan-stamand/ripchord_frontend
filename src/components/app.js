217a1b2f00ac354839dd08c0e2b05cfe944adeed
nathan-stamand <nathansaint810@gmail.com>
2020-07-01 13:54:50 -0500

    created #attemptCreateUser to make POST fetch request to api with username/password fields

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,31 @@ class App {
     this.songs = new Songs();
   }
 
+
+  static attemptCreateUser(username, password) {
+    const userData = {
+      user: {
+        username: username,
+        password: password
+    }}
+
+    const userObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData)
+    }
+
+    return fetch("http://localhost:3000/api/v1/users", userObj)
+    .then((res) => {
+      return res.json()
+    })
+    .then(json => {
+      console.log(json)
+    })
+  }
+  
   addCreateUserEventListener() {
     const loginSubmitBtn = document.getElementById("sign-up").querySelector("form").lastElementChild;
     loginSubmitBtn.addEventListener("click", function(e) {
