287828cbef9e019b6f12e0a876f63e22415d1a6c
nathan-stamand <nathansaint810@gmail.com>
2020-07-01 13:52:11 -0500

    created #addCreateUserEventListener to prevent default on Sign Up button and call #attemptCreateUser using values in username/ppassword inputs

--- src/components/app.js
+++ src/components/app.js
@@ -2,4 +2,14 @@ class App {
   constructor() {
     this.songs = new Songs();
   }
+
+  addCreateUserEventListener() {
+    const loginSubmitBtn = document.getElementById("sign-up").querySelector("form").lastElementChild;
+    loginSubmitBtn.addEventListener("click", function(e) {
+      e.preventDefault();
+      const username = document.getElementById("sign-up").querySelector("form").firstElementChild.value;
+      const password = document.getElementById("sign-up").querySelector("form").children[3].value;
+      App.attemptCreateUser(username, password)
+    })
+  }
 }
